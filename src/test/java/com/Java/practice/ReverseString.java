f5bff00010f4c0ad3ea8faefc8232fb9a7cdcfbe
Skinny Astro <sartapeprasad@gmail.com>
2025-03-19 17:30:23 +0530

    Code Changed

--- src/test/java/com/Java/practice/ReverseString.java
+++ src/test/java/com/Java/practice/ReverseString.java
@@ -1,5 +1,4 @@
 package com.Java.practice;
-
 public class ReverseString {
     public static void main(String[] args) {
         String str = "Prashik Dhule";
@@ -7,5 +6,17 @@ public class ReverseString {
         String reverse = sb.reverse().toString();
 
         System.out.println("Reverse string is " + reverse);
+        ReverseString RS = new ReverseString();
+        RS.UsingLoop();
+
+    }
+    public void UsingLoop(){
+        String str = "SDET";
+        String reverse = "";
+
+        for (int i = str.length()-1;i>=0 ; i--){
+             reverse+=str.charAt(i);
+            System.out.println(reverse);
+        }
     }
 }
