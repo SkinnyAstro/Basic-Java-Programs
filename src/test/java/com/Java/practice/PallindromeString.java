df00db19bbccf3e871bb5b10ea75d86c2c24dda2
Skinny Astro <sartapeprasad@gmail.com>
2025-03-20 10:22:18 +0530

    Pallindrome code added

--- src/test/java/com/Java/practice/PallindromeString.java
+++ src/test/java/com/Java/practice/PallindromeString.java
@@ -1,2 +1,14 @@
-package com.Java.practice;public class PallindromeString {
+package com.Java.practice;
+
+public class PallindromeString {
+
+    public static boolean isPallindrome(String str){
+        String rev = new StringBuilder(str).reverse().toString(); // we are creating an new rev variable of String type.
+        return rev.equals(str); // we are are returning an boolean value whether rev value is equal to str.
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPallindrome("Hello")); // this will return the value as false
+        System.out.println(isPallindrome("madam")); // this will return the value as true.
+    }
 }
--- src/test/java/com/Java/practice/ReverseString.java
+++ src/test/java/com/Java/practice/ReverseString.java
@@ -2,7 +2,7 @@ package com.Java.practice;
 public class ReverseString {
     public static void main(String[] args) {
         String str = "Prashik Dhule";
-        StringBuilder sb = new StringBuilder(str);
+        StringBuilder sb = new StringBuilder(str); 
         String reverse = sb.reverse().toString();
 
         System.out.println("Reverse string is " + reverse);
@@ -14,8 +14,8 @@ public class ReverseString {
         String str = "SDET";
         String reverse = "";
 
-        for (int i = str.length()-1;i>=0 ; i--){
-             reverse+=str.charAt(i);
+        for (int i = str.length()-1;i>=0 ; i--){ // str.lenth is an Java method which returns the number of characters in String.
+             reverse+=str.charAt(i); // charAT is an java method that returns character at an specified index.
             System.out.println(reverse);
         }
     }
