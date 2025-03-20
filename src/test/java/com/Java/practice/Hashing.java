fdb7e979818c5410c168dd9a79933247cc49fe41
Skinny Astro <sartapeprasad@gmail.com>
2025-03-20 15:48:30 +0530

    Hashmap code added

---
+++ src/test/java/com/Java/practice/Hashing.java
@@ -0,0 +1,16 @@
+
+package com.Java.practice;
+import java.util.*;
+
+public class Hashing {
+    public static void main(String[] args) {
+        //country<key> , population<value>
+        HashMap<String,Integer> map = new HashMap();
+
+        //Insertion.
+        map.put("India",200);
+        map.put("America",200);
+        System.out.println(map);
+    }
+
+}
