01d750bda25a911d3183462dabe60477ff277b04
Skinny Astro <sartapeprasad@gmail.com>
2025-03-26 09:47:59 +0530

    Changes

--- src/test/java/com/Java/practice/Hashing.java
+++ src/test/java/com/Java/practice/Hashing.java
@@ -7,13 +7,17 @@ public class Hashing {
         //country<key> , population<value>
         HashMap<String,Integer> map = new HashMap();
 
+
         //Insertion.
         map.put("India",200);
         map.put("America",200);
+        map.put("China",200);
         //System.out.println(map);
 
         map.put("India",300);
         //System.out.println(map);
+
+        // search operation.
         if (map.containsKey("Indonesia")){ // this will return true or false.
             System.out.println("Key is present");
         }else {
@@ -22,6 +26,37 @@ public class Hashing {
 
         System.out.println(map.get("India")); //this will return the value of key Inida.
         System.out.println(map.get("England")); // this will return null value as England key is not present.
+
+//        int arr[] = {12,15,13};
+//        for (int i=0 ; i<3 ; i++){  // using this type of loop we are printing the value of index
+//            System.out.print(arr[i]+  " ");
+//        }
+//        System.out.println();
+//
+//        for (int value : arr){ // using this type we are directly accessing the value of the index.
+//            System.out.print(value + " ");
+//        }
+//        System.out.println();
+
+        for (Map.Entry<String,Integer> e:map.entrySet()){
+            System.out.println(e.getKey());
+            System.out.println(e.getValue());
+        }
+
+        Set<String>keys = map.keySet();
+        for (String key : keys){
+            System.out.println(key+" "+map.get(key));
+        }
+
+        map.size();
+
+
+
+
+        // removing an value from the map.
+//        map.remove("China");
+//        System.out.println(map);
+
 
 //        HashMap<String,Integer> map1 = new HashMap();
 //        map1.put("Petrol",20);
