dfd224e0d2ef1c23e786bd02ae5ab1d12d493994
Skinny Astro <sartapeprasad@gmail.com>
2025-03-27 09:36:24 +0530

    New added

--- src/test/java/com/Java/practice/Fibonacci.java
+++ src/test/java/com/Java/practice/Fibonacci.java
@@ -1,20 +1,15 @@
 package com.Java.practice;
-
 public class Fibonacci {
     public static void main(String[] args) {
-        int n = 10;
-        int first = 0 , second =1;
-        System.out.print("Fibonacci number upto "+n +"terms: ");
-
-        for (int i =0;i<n;i++){
-            System.out.print(first +" ");
-
+       int n = 10;
+       int first = 0 , second = 1;
+        System.out.println("Fibonacci number upto "+ n + "terms: ");
+        for (int i=0 ; i<n ; i++) {
+            System.out.print(first + " ");
             int next = first+second;
-            first=second;
-            second =next;
-
+            first= second;
+            second= next;
         }
 
-
- }
+    }
 }
